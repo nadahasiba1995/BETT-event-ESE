d44ce4b58e7080813ce540f495f97ec8968539d3
nadahasiba <nada@uaedigitallab.com>
2022-03-19 20:19:58 +0400

    initiate

---
+++ js/fadein.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $(this).scrollTop(0);
+
+    $('#section-1').fadeIn('slow');
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.hideme').each( function(i){
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight()/2;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+           
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},1500);
+                    
+            }
+            
+        }); 
+    
+    });
+    
+});
+
